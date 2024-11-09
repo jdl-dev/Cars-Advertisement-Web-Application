de1eb6562c11790d3595becc6976dbf30bd9ed03
Daniel <januszkiewiczdl@gmail.com>
2024-11-09 23:45:14 +0100

    Initial sketch of Car class.

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.time.Year;
+
+public class Car {
+    private Long id;
+    private int price;
+    private int mileage;
+    private Brand brand;
+    private Petrol petrol;
+    private Gearbos gearbox;
+    private Bodytype bodytype;
+    private int displacement;
+    private int power;
+    private String description;
+    private LocalDateTime dateOfAddingTheAdd;
+    private Year yearOfProduction;
+    private int doorNumber;
+    private int amountOfSeats;
+    private ColorPalette color;
+    private String vin;
+    private Condition condition;
+}
