fb68d41cdb18e25e04b6b7072c25f37ffeea5106
Daniel <januszkiewiczdl@gmail.com>
2024-11-10 21:31:34 +0100

    Controller creation.

---
+++ src/main/java/controller/CarController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import service.CarService;
+
+@Controller
+public class CarController {
+    private CarService carService;
+}
