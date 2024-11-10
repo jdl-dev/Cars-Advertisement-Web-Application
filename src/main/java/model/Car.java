1dff75fd1ff64f92f6265ab40e7cad0fd250d5c9
Daniel <januszkiewiczdl@gmail.com>
2024-11-10 17:41:53 +0100

    Validation of every Car member implemented. Can have bugs, sketch for now.

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -6,7 +6,19 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import model.validation.annoations.ValidAmountOfSeats;
+import model.validation.annoations.ValidDateOfAddingTheAdd;
+import model.validation.annoations.ValidDescription;
+import model.validation.annoations.ValidDisplacement;
+import model.validation.annoations.ValidDoorNumbers;
+import model.validation.annoations.ValidEnumMembersOfCarClass;
+import model.validation.annoations.ValidMileage;
+import model.validation.annoations.ValidPower;
+import model.validation.annoations.ValidPrice;
+import model.validation.annoations.ValidVin;
+import model.validation.annoations.ValidYearOfProduction;
 
+import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.Year;
 
@@ -15,26 +27,58 @@ public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ValidPrice
     private int price;
+
+    @ValidMileage
     private int mileage;
+
     @Enumerated(value = EnumType.STRING)
+    @ValidEnumMembersOfCarClass(enumClass = Brand.class)
     private Brand brand;
+
     @Enumerated(value = EnumType.STRING)
+    @ValidEnumMembersOfCarClass(enumClass = Petrol.class)
     private Petrol petrol;
+
     @Enumerated(value = EnumType.STRING)
+    @ValidEnumMembersOfCarClass(enumClass = Gearbos.class)
     private Gearbos gearbox;
+
     @Enumerated(value = EnumType.STRING)
+    @ValidEnumMembersOfCarClass(enumClass = Bodytype.class)
     private Bodytype bodytype;
+
+    @ValidDisplacement
     private int displacement;
+
+    @ValidPower
     private int power;
+
+    @ValidDescription
     private String description;
+
+    @ValidDateOfAddingTheAdd
     private LocalDateTime dateOfAddingTheAdd;
+
+    @ValidYearOfProduction
     private Year yearOfProduction;
+
+    @ValidDoorNumbers
     private int doorNumber;
+
+    @ValidAmountOfSeats
     private int amountOfSeats;
+
     @Enumerated(value = EnumType.STRING)
+    @ValidEnumMembersOfCarClass(enumClass = ColorPalette.class)
     private ColorPalette color;
+
+    @ValidVin
     private String vin;
+
     @Enumerated(value = EnumType.STRING)
+    @ValidEnumMembersOfCarClass(enumClass = Condition.class)
     private Condition condition;
 }
