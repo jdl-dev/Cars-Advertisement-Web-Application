bc2955d24d74f785e4de9002068bedc51f0b4efd
Daniel <januszkiewiczdl@gmail.com>
2024-11-10 11:54:12 +0100

    Description validator.

---
+++ src/main/java/model/validation/DescriptionValidator.java
@@ -0,0 +1,27 @@
+package model.validation;
+
+import model.validation.annoations.ValidDescription;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class DescriptionValidator implements ConstraintValidator<ValidDescription, String> {
+    int maxDescriptionLength;
+
+    @Override
+    public void initialize(ValidDescription constraintAnnotation) {
+        this.maxDescriptionLength = constraintAnnotation.maxDescriptionLength();
+    }
+
+    @Override
+    public boolean isValid(String description, ConstraintValidatorContext context) {
+        if (description.length() > maxDescriptionLength) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Description length is to long. Maxial description length is " + description)
+                    .addConstraintViolation();
+            return false;
+        }
+
+        return true;
+    }
+}
