d9a3c4a1a9e6a018d50789ff388e490647e3eea7
Daniel <januszkiewiczdl@gmail.com>
2024-11-10 10:16:26 +0100

    Added basic annotations in Car entity.

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,15 +1,29 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 import java.time.Year;
 
+@Entity
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int price;
     private int mileage;
+    @Enumerated(value = EnumType.STRING)
     private Brand brand;
+    @Enumerated(value = EnumType.STRING)
     private Petrol petrol;
+    @Enumerated(value = EnumType.STRING)
     private Gearbos gearbox;
+    @Enumerated(value = EnumType.STRING)
     private Bodytype bodytype;
     private int displacement;
     private int power;
@@ -18,7 +32,9 @@ public class Car {
     private Year yearOfProduction;
     private int doorNumber;
     private int amountOfSeats;
+    @Enumerated(value = EnumType.STRING)
     private ColorPalette color;
     private String vin;
+    @Enumerated(value = EnumType.STRING)
     private Condition condition;
 }
