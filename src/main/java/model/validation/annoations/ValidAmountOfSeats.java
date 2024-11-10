e9b9b2be407f966d9d2b58897f9f7d505d78f0be
Daniel <januszkiewiczdl@gmail.com>
2024-11-10 17:40:50 +0100

    Seats validator.

---
+++ src/main/java/model/validation/annoations/ValidAmountOfSeats.java
@@ -0,0 +1,22 @@
+package model.validation.annoations;
+
+import model.validation.AmountOfSeatsValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = AmountOfSeatsValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidAmountOfSeats {
+    String message() default "Amount of seats is not proper";
+
+    int minSeats() default 1;
+
+    int maxSeats() default 100;
+}
