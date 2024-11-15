9fb1bc0500d2094fdf7b45086f4b08e7388fdf7f
Daniel <januszkiewiczdl@gmail.com>
2024-11-15 19:34:03 +0100

    Removed unnecesaarry code.

--- src/main/java/com/app/myapp/model/Car.java
+++ src/main/java/com/app/myapp/model/Car.java
@@ -17,7 +17,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Min;
 
 import java.time.LocalDateTime;
 import java.time.Year;
