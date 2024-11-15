4d46d7649878cf22c69a1967056d4ad8937a40f8
Daniel <januszkiewiczdl@gmail.com>
2024-11-15 19:32:29 +0100

    Added validating enum classes.

--- src/main/java/com/app/myapp/model/validation/annoations/ValidEnumMembersOfCarClass.java
+++ src/main/java/com/app/myapp/model/validation/annoations/ValidEnumMembersOfCarClass.java
@@ -1,5 +1,10 @@
 package com.app.myapp.model.validation.annoations;
 
+import com.app.myapp.model.validation.EnumBodyTypeValidator;
+import com.app.myapp.model.validation.EnumBrandValidator;
+import com.app.myapp.model.validation.EnumColorPaletteValidator;
+import com.app.myapp.model.validation.EnumGearboxValidator;
+import com.app.myapp.model.validation.EnumPetrolValidator;
 import jakarta.validation.Constraint;
 
 import java.lang.annotation.Documented;
@@ -9,7 +14,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
-@Constraint(validatedBy = {})
+@Constraint(validatedBy = {
+        EnumBrandValidator.class,
+        EnumPetrolValidator.class,
+        EnumGearboxValidator.class,
+        EnumBodyTypeValidator.class,
+        EnumColorPaletteValidator.class
+})
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEnumMembersOfCarClass {
