874bdf3949584a425fb2825d8d7fe9bbed0f9459
Daniel <januszkiewiczdl@gmail.com>
2025-01-18 16:34:47 +0100

    Cors global configuration.

---
+++ src/main/java/com/app/myapp/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.app.myapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    //The example above enables CORS requests from any origin to any endpoint in the application.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
