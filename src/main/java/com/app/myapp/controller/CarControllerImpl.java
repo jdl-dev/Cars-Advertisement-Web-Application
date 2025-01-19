5ee6eb1fef5470255b741fce9ea9c69ebbebf1ac
Daniel <januszkiewiczdl@gmail.com>
2025-01-19 11:28:19 +0100

    CarController created and implemented.

--- src/main/java/com/app/myapp/controller/CarControllerImpl.java
+++ src/main/java/com/app/myapp/controller/CarControllerImpl.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
-public class CarControllerImpl {
+public class CarControllerImpl implements CarController {
 
     private final CarService carService;
 
