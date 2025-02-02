935a2b728c888fa2259abddfed6ac80cb499ddff
Daniel <januszkiewiczdl@gmail.com>
2025-02-03 00:13:27 +0100

    User change password DTO added

--- src/main/java/com/app/myapp/dto/user_dtos/UserChangePasswordDto.java
+++ src/main/java/com/app/myapp/dto/user_dtos/UserChangePasswordDto.java
@@ -1,9 +1,12 @@
 package com.app.myapp.dto.user_dtos;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class UserChangePasswordDto {
+    @NotNull
     private String oldPassword;
+    @NotNull
     private String newPassword;
 }
