ed5533455968c1f412e085c979cc797d883ceb1c
Daniel <januszkiewiczdl@gmail.com>
2025-02-02 20:26:29 +0100

    Removed cors settings for now.

---
+++ src/main/java/com/app/myapp/config/WebCorsConfig.java
@@ -0,0 +1,16 @@
+package com.app.myapp.config;
+
+//@Configuration
+////@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    // The example above enables CORS requests from any origin to any endpoint in the application.
+////    @Override
+////    public void addCorsMappings(CorsRegistry registry) {
+////        registry.addMapping("/**")
+////                .allowedOrigins("http://localhost:4200")
+////                .allowedMethods("GET", "POST", "PUT", "DELETE");
+////    }
+//
+//
+//}
