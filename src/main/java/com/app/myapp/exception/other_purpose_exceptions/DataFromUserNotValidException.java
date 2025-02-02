d2090a798531596bf098fc3be96891feb6dc2e6b
Daniel <januszkiewiczdl@gmail.com>
2025-02-03 00:11:32 +0100

    Added DateFromUserNotValidException exception.

--- src/main/java/com/app/myapp/exception/other_purpose_exceptions/DataFromUserNotValidException.java
+++ src/main/java/com/app/myapp/exception/other_purpose_exceptions/DataFromUserNotValidException.java
@@ -2,12 +2,10 @@ package com.app.myapp.exception.other_purpose_exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
 @Getter
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class DataFromUserNotValidException extends RuntimeException {
     private final Set<String> errorMessages;
