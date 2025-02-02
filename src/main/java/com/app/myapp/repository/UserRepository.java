cd6d9e1e3daa418ef739cf5a38866823725b8ad4
Daniel <januszkiewiczdl@gmail.com>
2025-02-02 16:46:27 +0100

    Optional<User> findUserByPesel(String pesel); added

--- src/main/java/com/app/myapp/repository/UserRepository.java
+++ src/main/java/com/app/myapp/repository/UserRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByPesel(String pesel);
+
     @Query(value = "SELECT email FROM cars_database.user", nativeQuery = true)
     List<String> findTableWithEmails();
 
