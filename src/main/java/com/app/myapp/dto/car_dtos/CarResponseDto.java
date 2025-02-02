6395fcc7217e1a23cbc653a00eedead2f4607a90
Daniel <januszkiewiczdl@gmail.com>
2025-02-02 20:28:02 +0100

    Added NoArgsConstructor       @AllArgsConstructor       @Getter to CarResponseDto

--- src/main/java/com/app/myapp/dto/car_dtos/CarResponseDto.java
+++ src/main/java/com/app/myapp/dto/car_dtos/CarResponseDto.java
@@ -7,12 +7,18 @@ import com.app.myapp.model.model.car_members.ColorPalette;
 import com.app.myapp.model.model.car_members.Gearbox;
 import com.app.myapp.model.model.car_members.Petrol;
 import com.app.myapp.model.model.car_members.State;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.time.Year;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 @Builder
 @ToString
 public class CarResponseDto {
